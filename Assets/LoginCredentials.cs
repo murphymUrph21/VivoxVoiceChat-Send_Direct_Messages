fed893a684af8b889f60b4a13ad39db02b16de54
murphymUrph21 <68832220+murphymUrph21@users.noreply.github.com>
2020-12-15 13:00:02 -0800

    1st commit

--- Assets/LoginCredentials.cs
+++ Assets/LoginCredentials.cs
@@ -15,11 +15,15 @@ public class LoginCredentials : MonoBehaviour
     private string  domain = "mt1s.vivox.com";
     private string tokenKey = "luck408";
     private TimeSpan timeSpan = new TimeSpan(90);
+
+
     private ILoginSession loginSession;
+    private IChannelSession channelSession;
 
 
     #region UI Variables
-
+    // Updated UI variables not shown In my first video
+    // The rest of the code is fairly the same
     [SerializeField] Text txt_UserName;
     [SerializeField] Text txt_ChannelName;
     [SerializeField] Text txt_Message_Prefab;
@@ -47,7 +51,19 @@ public class LoginCredentials : MonoBehaviour
 
     void Start()
     {
-      //  Instantiate(txtMessage, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
+       Instantiate(txt_Message_Prefab, container.transform);
 
     }
 
@@ -61,12 +77,26 @@ public class LoginCredentials : MonoBehaviour
         {
             loginSesh.PropertyChanged -= Login_Status;
         }
-
     }
+
+    public void Bind_Channel_Callback_Listeners(bool bind, IChannelSession channelSesh)
+    {
+        if (bind)
+        {
+            channelSesh.PropertyChanged += On_Channel_Status_Changed;
+        }
+        else
+        {
+            channelSesh.PropertyChanged -= On_Channel_Status_Changed;
+        }
+    }
+
+
+    #region Login Methods
 
     public void LoginUser()
     {
-        Login("Test_Name");
+        Login(tmp_Input_Username.text);
     }
 
     public void Login(string userName)
@@ -108,8 +138,80 @@ public class LoginCredentials : MonoBehaviour
             
             case LoginState.LoggedIn:
                 Debug.Log($"Logged In {loginSession.LoginSessionId.Name}");
+                txt_UserName.text = loginSession.LoginSessionId.Name;
                 break;
         }
     }
+
+
+
+    #endregion
+
+
+    #region Join Channel Methods
+
+    public void JoinChannel(string channelName, bool IsAudio, bool IsText, bool switchTransmission,ChannelType channelType)
+    {
+
+        ChannelId channelId = new ChannelId(issuer, channelName, domain, channelType);
+        channelSession = loginSession.GetChannelSession(channelId);
+        Bind_Channel_Callback_Listeners(true, channelSession);
+
+        channelSession.BeginConnect(IsAudio, IsText, switchTransmission, channelSession.GetConnectToken(tokenKey, timeSpan), ar => 
+        {
+            try
+            {
+                channelSession.EndConnect(ar);
+            }
+            catch(Exception e)
+            {
+                Bind_Channel_Callback_Listeners(false, channelSession);
+                Debug.Log(e.Message);
+            }
+        });
+    }
+
+
+    public void Btn_Join_Channel()
+    {
+        JoinChannel(tmp_Input_ChannelName.text, true, true, true, ChannelType.NonPositional);
+    }
+
+    public void Leave_Channel(IChannelSession channelToDiconnect, string channelName)
+    {
+        channelToDiconnect.Disconnect();
+        loginSession.DeleteChannelSession(new ChannelId(issuer, channelName, domain));
+    }
+
+    public void Btn_Leave_Channel_Clicked()
+    {
+        Leave_Channel(channelSession, tmp_Input_ChannelName.text);
+    }
+
+    public void On_Channel_Status_Changed(object sender, PropertyChangedEventArgs channelArgs)
+    {
+        IChannelSession source = (IChannelSession)sender;
+
+        switch (source.ChannelState)
+        {
+            case ConnectionState.Connecting:
+                Debug.Log("Channel Connecting");
+                break;    
+            case ConnectionState.Connected:
+                Debug.Log($"{source.Channel.Name} Connected");
+                txt_ChannelName.text = source.Channel.Name;
+                break;       
+            case ConnectionState.Disconnecting:
+                Debug.Log($"{source.Channel.Name} disconnecting");
+                break;    
+            case ConnectionState.Disconnected:
+                Debug.Log($"{source.Channel.Name} disconnected");
+                break;
+        }
+    }
+
+    #endregion
+
+
 
 }
